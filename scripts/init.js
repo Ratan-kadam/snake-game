d232c92183072b3792c60a6792a85518fa1287d4
Ratan Kadam <ratankadam@Ratans-MacBook-Pro.local>
2020-05-31 14:25:21 -0400

    making snake as singleton class

---
+++ scripts/init.js
@@ -0,0 +1,4 @@
+// all global varibales required for all other scripts
+const canvas = document.getElementById('canvas');
+const context = canvas.getContext("2d");
+const snake1 = new snake('Cobra');
--- scripts/snake.js
+++ scripts/snake.js
@@ -1,1 +1,34 @@
+/*
+  As we want only one instance of snake in game
+  making the snake class as singleton;
 
+  to verify: put below lines in init.js
+
+  const snake1 = new snake('cobra');
+  const snake2 = new snake('python');
+  console.log("snake name:", snake1.name); // output cobra
+  console.log("snake1 name:", snake2.name); // outout cobra
+
+*/
+
+function snake (name) {
+  'use strict'
+
+  if (!!snake.instance) {
+    return snake.instance;
+  }
+
+  var instance;
+
+  this.name = name || 'Anaconda';
+
+  function getName () {
+    return name;
+  }
+
+  snake.instance = this;
+
+  return {
+    name: getName(),
+  }
+};
