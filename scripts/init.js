ad32662ffde746fe71f2576b42f664f7cd1392b9
Ratan Kadam <ratankadam@Ratans-MacBook-Pro.local>
2020-05-31 17:59:45 -0400

    Adding snake class and logic to move

--- scripts/init.js
+++ scripts/init.js
@@ -1,4 +1,26 @@
-// all global varibales required for all other scripts
+// main executor all global varibales required for all other scripts
 const canvas = document.getElementById('canvas');
 const context = canvas.getContext("2d");
-const snake1 = new snake('Cobra');
+const cobra = new snake('Cobra');
+context.fillStyle = "#D1FF04";
+const canvasWidth = canvas.width;
+const canvasHeight = canvas.height;
+
+const DIRECTION = {
+  UP: 'ArrowUp',
+  DOWN: 'ArrowDown',
+  RIGHT: 'ArrowRight',
+  LEFT: 'ArrowLeft'
+};
+
+(function() {
+  window.addEventListener('keyup', function(e) {
+    const { key } = e;
+    cobra.changeDirection(key);
+  });
+
+  setInterval(function() {
+    context.clearRect(0,0,canvasWidth,canvasHeight);
+    cobra.drawSnake()
+  }, 230);
+})();
--- scripts/snake.js
+++ scripts/snake.js
@@ -8,7 +8,6 @@
   const snake2 = new snake('python');
   console.log("snake name:", snake1.name); // output cobra
   console.log("snake1 name:", snake2.name); // outout cobra
-
 */
 
 function snake (name) {
@@ -17,18 +16,66 @@ function snake (name) {
   if (!!snake.instance) {
     return snake.instance;
   }
-
-  var instance;
+  snake.instance = this;
 
   this.name = name || 'Anaconda';
+  let x=0;
+  let y=0;
+  const height = 10;
+  const width = 10;
+  const displacementFactor = 10;
+  let currentDirection;
 
   function getName () {
     return name;
   }
 
-  snake.instance = this;
+  function getX() {
+    return x;
+  }
+
+  function getY() {
+    return y;
+  }
+
+  function updateCordinatesAuto() {
+    let newX;
+    let newY;
+    switch (currentDirection) {
+      case DIRECTION.UP:
+          newY = y - displacementFactor;
+          y = newY < 0 ? canvasHeight : newY;
+        break;
+      case DIRECTION.DOWN:
+          newY = y + displacementFactor;
+          y = newY > canvas.height ? 0 : newY;
+        break;
+      case DIRECTION.RIGHT:
+          newX = x + displacementFactor;
+          x = newX > canvas.width ? 0 : newX;
+        break;
+      case DIRECTION.LEFT:
+          newX = x - displacementFactor;
+          x = newX < 0 ? canvas.width : newX;
+        break;
+      default:
+    }
+  }
+
+  function drawSnake() {
+    updateCordinatesAuto();
+    context.fillRect(getX(),getY(),height, width);
+  }
+
+  function changeDirection(direction) {
+    currentDirection = direction
+  }
 
   return {
     name: getName(),
+    x: getX(),
+    y: getY(),
+    drawSnake: drawSnake,
+    changeDirection: changeDirection,
   }
 };
